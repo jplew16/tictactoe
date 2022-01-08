9f779cfd3853ff72ad916801ea7ef86035f4fb4f
Jonathan Plewinski <j.plewinski@yahoo.com>
2022-01-08 13:52:11 -0700

    Added basic new player form :)

---
+++ script.js
@@ -0,0 +1,9 @@
+function Player(name, marker) {
+    return {name, marker};
+} 
+document.addEventListener('form');
+
+((player) => {
+    const board = [null, null, null, null, null, null, null, null, null];
+    
+})
