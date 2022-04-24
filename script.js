c38df39b9a54b58d6ba2942e5fa2cf4c1ae31be5
Jonathan Plewinski <j.plewinski@yahoo.com>
2022-04-23 23:24:49 -0600

    Removed event module. Added player move action on open spaces :)

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-function Player(name, marker) {
-    return {name, marker};
-} 
-document.addEventListener('form');
+const gameboard = (() => {
+    let board = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
+    let blankSpaces = [];
+    let player1 = {};
+    let playerCom = {};
 
-((player) => {
-    const board = [null, null, null, null, null, null, null, null, null];
-    
-})
+    const playerPlace = function(pos) {
+        board[pos] = gameboard.player1.marker;
+        blankSpaces.push(pos);
+        console.log(board);
+        console.log(blankSpaces);
+    }
+    const comPlace = function() {
+
+    }
+    return {
+        player1,
+        playerPlace
+    }
+})();
+
+const displayController = (() =>{
+    const placeMarker = (e) => {
+        let playerPos = e['target']['dataset']['pos'];
+        let posEl = displayBoard.querySelector(`[data-pos='${playerPos}']`);
+
+            if (e.target.tagName == 'TD' && posEl.textContent != gameboard.player1.marker){
+                
+                gameboard.playerPlace(playerPos);
+                posEl.textContent = gameboard.player1.marker;
+            }    
+} 
+return {
+    placeMarker
+}
+})();
+
+const makePlayer = function(name, marker) {
+    return {name, marker};
+}
+window.addEventListener('load', function() {
+    gameboard.player1 = makePlayer('John', 'O');
+    gameboard.playerCom = makePlayer('CPU', 'X');
+});
+
+let displayBoard = document.querySelector('div.gameboard');
+displayBoard.addEventListener('click', displayController.placeMarker);
+
