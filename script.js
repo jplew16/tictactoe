c61de2a653c5f5f18ddcf96a8d759c683dd5e163
Jonathan Plewinski <j.plewinski@yahoo.com>
2022-04-24 21:59:16 -0600

    Encapsulated dom logic in a module. Tied gameboard, display, and dom modules based on event handlers :)

--- script.js
+++ script.js
@@ -1,48 +1,61 @@
 const gameboard = (() => {
     let board = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
-    let blankSpaces = [];
+    let emptySpaces = [0,1,2,3,4,5,6,7,8];
     let player1 = {};
     let playerCom = {};
-
+    
     const playerPlace = function(pos) {
         board[pos] = gameboard.player1.marker;
-        blankSpaces.push(pos);
+        emptySpaces.splice(pos, 1);
         console.log(board);
-        console.log(blankSpaces);
+        console.log(emptySpaces);
+    }
+    const makePlayer = function(name, marker) {
+        return {name, marker};
     }
     const comPlace = function() {
+        let choice = Math.floor(Math.random() * emptySpaces.length);
+        board[choice] = gameboard.playerCom.marker;
+        emptySpaces.splice(choice, 1);
+        console.log(board);
+        console.log(emptySpaces);
 
     }
     return {
         player1,
-        playerPlace
+        playerCom,
+        playerPlace,
+        makePlayer
     }
 })();
 
 const displayController = (() =>{
     const placeMarker = (e) => {
         let playerPos = e['target']['dataset']['pos'];
-        let posEl = displayBoard.querySelector(`[data-pos='${playerPos}']`);
-
-            if (e.target.tagName == 'TD' && posEl.textContent != gameboard.player1.marker){
-                
-                gameboard.playerPlace(playerPos);
-                posEl.textContent = gameboard.player1.marker;
-            }    
-} 
-return {
-    placeMarker
-}
+        let posEl = dom.displayBoard.querySelector(`[data-pos='${playerPos}']`);
+        
+        if (e.target.tagName == 'TD' && posEl.textContent != gameboard.player1.marker){
+            
+            gameboard.playerPlace(playerPos);
+            posEl.textContent = gameboard.player1.marker;
+        }    
+    } 
+    return {
+        placeMarker
+    }
 })();
 
-const makePlayer = function(name, marker) {
-    return {name, marker};
-}
-window.addEventListener('load', function() {
-    gameboard.player1 = makePlayer('John', 'O');
-    gameboard.playerCom = makePlayer('CPU', 'X');
-});
+const dom = (function(global) {
+    let displayBoard = global.document.querySelector('div.gameboard');
+    displayBoard.addEventListener('click', displayController.placeMarker);
+    
+    global.addEventListener('load', function() {
+        gameboard.player1 = gameboard.makePlayer('User', 'O');
+        gameboard.playerCom = gameboard.makePlayer('CPU', 'X');
+    });
+    return {
+        displayBoard
+    }
+})(window);
 
-let displayBoard = document.querySelector('div.gameboard');
-displayBoard.addEventListener('click', displayController.placeMarker);
 
