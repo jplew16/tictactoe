a1a3226c40407a4319f8ac6f8eff0146befe2025
Jonathan Plewinski <j.plewinski@yahoo.com>
2022-04-25 20:43:42 -0600

    Added claiming spot function connected to dom. Added computer opponent that claims random open spots :)

--- script.js
+++ script.js
@@ -1,58 +1,119 @@
 const gameboard = (() => {
-    let board = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
+    let board = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'];
     let emptySpaces = [0,1,2,3,4,5,6,7,8];
     let player1 = {};
     let playerCom = {};
     
-    const playerPlace = function(pos) {
-        board[pos] = gameboard.player1.marker;
-        emptySpaces.splice(pos, 1);
-        console.log(board);
-        console.log(emptySpaces);
-    }
     const makePlayer = function(name, marker) {
         return {name, marker};
     }
+    const check3InRow = function(playerName) {
+        let cycle = 3;
+        //horizontal 3 in a row check
+        for (let i=0; i<3; i++) {
+                if (board[cycle] == board[++cycle] && board[cycle] == board[++cycle]) {
+                    
+                }
+        }
+        cycle = 3
+        //vertical 3 in a row check
+        for (let i=0; i<3; i++) {
+            if (board[cycle] == board[++cycle] && board[cycle] == board[++cycle]) {
+                //winner
+            }
+    }
+        cycle = 2
+        //diagonal 3 in a row check
+        for (let i=0; i<2; i++) {
+            if (board[cycle] == board[++cycle] && board[cycle] == board[++cycle]) {
+                //winner
+            }
+    }
+}
+
+
+    //Roll a random number out of available empty spots
+    let rollChoice = function() { 
+        return Math.floor(Math.random() * emptySpaces.length);
+    }
+    let findPosIndex = function(number) {
+            return number === +this;
+        }
+    const playerPlace = function(e) {
+        let playerPos = e['target']['dataset']['pos'];
+            
+            //Checks if clicked board spot is taken
+        if (board[playerPos] !== gameboard.player1.marker && 
+            board[playerPos] !== gameboard.playerCom.marker) {
+
+            board[playerPos] = gameboard.player1.marker; 
+
+            //Removes claimed spot index from empty spot list    
+            emptySpaces.splice(emptySpaces
+                .findIndex(findPosIndex.bind(playerPos))
+                , 1);
+            console.log(emptySpaces);
+            displayController.playerPlace(playerPos, gameboard.player1.marker)
+            comPlace();
+            check3InRow(gameboard.player1);
+            }
+        }
     const comPlace = function() {
-        let choice = Math.floor(Math.random() * emptySpaces.length);
-        board[choice] = gameboard.playerCom.marker;
-        emptySpaces.splice(choice, 1);
+        if (emptySpaces.length == 0) { 
+            //end game function here
+            check3InRow(gameboard.playerCom);
+        }
+        
+        let comPos = rollChoice();
+        console.log(comPos);
+        board[emptySpaces[comPos]] = gameboard.playerCom.marker;
+        
+        
+        displayController.playerPlace(emptySpaces[comPos], gameboard.playerCom.marker);
+        emptySpaces.splice(comPos, 1);
+        check3InRow(gameboard.playerCom);
+        
         console.log(board);
         console.log(emptySpaces);
 
     }
     return {
-        player1,
-        playerCom,
         playerPlace,
-        makePlayer
+        makePlayer, 
+        player1,
+        playerCom
     }
 })();
 
 const displayController = (() =>{
-    const placeMarker = (e) => {
-        let playerPos = e['target']['dataset']['pos'];
-        let posEl = dom.displayBoard.querySelector(`[data-pos='${playerPos}']`);
-        
-        if (e.target.tagName == 'TD' && posEl.textContent != gameboard.player1.marker){
-            
-            gameboard.playerPlace(playerPos);
-            posEl.textContent = gameboard.player1.marker;
-        }    
+    let getPosEl = function(pos) {
+        let board = dom.displayBoard.querySelectorAll('td');
+        let boardSpots = Array.from(board);
+
+        return boardSpots.find(function(spot) {
+            return spot['dataset']['pos'] == pos;
+        });
+    }
+    const playerPlace = (pos, marker) => {
+        let posEl = getPosEl(pos);
+            posEl.textContent = marker
     } 
     return {
-        placeMarker
+        playerPlace
     }
 })();
 
 const dom = (function(global) {
     let displayBoard = global.document.querySelector('div.gameboard');
-    displayBoard.addEventListener('click', displayController.placeMarker);
+    displayBoard.addEventListener('click', gameboard.playerPlace);
     
     global.addEventListener('load', function() {
         gameboard.player1 = gameboard.makePlayer('User', 'O');
         gameboard.playerCom = gameboard.makePlayer('CPU', 'X');
     });
+    let displayWinner = function() {
+        //toggle class for element showing winner
+    }
     return {
         displayBoard
     }
